ff7f92ca420aa8f5e1ebace350edc9822ba9a7f6
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-10-17 17:41:04 +0800

    Change date format

--- src/main/java/seedu/nuscents/data/Allowance.java
+++ src/main/java/seedu/nuscents/data/Allowance.java
@@ -1,9 +1,9 @@
 package seedu.nuscents.data;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 public class Allowance extends Transaction {
-    public Allowance(String amount, LocalDateTime date, String description, String additionalInfo) {
+    public Allowance(String amount, Date date, String description, String additionalInfo) {
         super(amount, date, description, additionalInfo);
     }
 }
