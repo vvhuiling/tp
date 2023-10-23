ac156217a18d0041fb306ddc01b08236e90f908a
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-10-23 23:19:49 +0800

    Add unknown category message

--- src/main/java/seedu/nuscents/ui/Messages.java
+++ src/main/java/seedu/nuscents/ui/Messages.java
@@ -1,7 +1,8 @@
 package seedu.nuscents.ui;
 
 public class Messages {
-    public static final String LINE = "-------------------------------------------------------------------------------";
+    public static final String LINE = "-------------------------------------------------------------------------------"
+            + "--------------";
     public static final String LOGO = " __   __  __   __   ____   _____ _____ __   __ ________   ____\n"
             + "|  \\ |  |  |  |  |/     / /   __|     |  \\ |  |__    __|/     /\n"
             + "|   \\|  |  |  |  |\\   __\\|   /  |   __|   \\|  |  |  |   \\   __\\\n"
@@ -25,15 +26,8 @@ public class Messages {
                     "- `exit` :" + System.lineSeparator() +
                     "    Exit the program.";
     public static final String MESSAGE_UNKNOWN = "OOPS!!! I'm sorry, but I don't know what that means :-(";
-    public static final String MESSAGE_MARK = "Nice! I've marked this task as done:";
-    public static final String MESSAGE_UNMARK = "OK, I've marked this task as not done yet:";
-    public static final String MESSAGE_EMPTY_TODO = "OOPS!!! The description of a todo cannot be empty.";
     public static final String MESSAGE_EMPTY_ALLOWANCE = "OOPS!!! Invalid input format for adding an allowance.";
     public static final String MESSAGE_EMPTY_EXPENSE = "OOPS!!! Invalid input format for adding an expense.";
-    public static final String MESSAGE_EMPTY_BY = "OOPS!!! The /by parameter of a deadline cannot be empty.";
-    public static final String MESSAGE_EMPTY_EVENT = "OOPS!!! The description of an event cannot be empty.";
-    public static final String MESSAGE_EMPTY_FROM = "OOPS!!! The /from parameter of an event cannot be empty.";
-    public static final String MESSAGE_EMPTY_TO = "OOPS!!! The /to parameter of an event cannot be empty.";
     public static final String MESSAGE_INVALID_AMOUNT = "OOPS!!! The amount entered needs to be a float!";
     public static final String MESSAGE_INVALID_DATE = "OOPS!!! The format of the date is invalid.";
     public static final String MESSAGE_INVALID_INDEX = "OOPS!!! This is an invalid transaction index.";
@@ -43,4 +37,6 @@ public class Messages {
     public static final String MESSAGE_EMPTY_INDEX = "OOPS!!! The index of a delete/view command cannot be empty.";
     public static final String MESSAGE_FATAL_ERROR = "OOPS!!! Fatal error occurred...";
     public static final String MESSAGE_READ_ERROR = "OOPS!!! A problem occurred while reading the data file.";
+    public static final String MESSAGE_UNKNOWN_CATEGORY = "OOPS!!! The category you provided is not a valid category\n."
+            + "Valid categories are FOOD/ENTERTAINMENT/TRANSPORTATION/UTILITY/RENT/OTHERS.";
 }
