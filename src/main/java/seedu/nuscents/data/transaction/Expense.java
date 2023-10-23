d83ef516eb453895f47baa6f018732334ae31b62
ChungZhiXuan <zhixuanchung03@gmail.com>
2023-10-23 23:17:46 +0800

    Add enum for expense categories

--- src/main/java/seedu/nuscents/data/transaction/Expense.java
+++ src/main/java/seedu/nuscents/data/transaction/Expense.java
@@ -1,9 +1,10 @@
-package seedu.nuscents.data;
+package seedu.nuscents.data.transaction;
 
 import java.util.Date;
 
 public class Expense extends Transaction {
-    public Expense(float amount, Date date, String description, String additionalInfo) {
-        super(amount, date, description, additionalInfo);
+
+    public Expense(float amount, Date date, String description, String additionalInfo, ExpenseCategory category) {
+        super(amount, date, description, additionalInfo, category);
     }
 }
