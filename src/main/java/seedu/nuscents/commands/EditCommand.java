3864750bdc20e5570775f5750d0a4d2166024a01
XuJiaChen_Aaron <57344818+aaronxujiachen@users.noreply.github.com>
2023-11-01 21:34:15 +0800

    Update EditCommand.java

--- src/main/java/seedu/nuscents/commands/EditCommand.java
+++ src/main/java/seedu/nuscents/commands/EditCommand.java
@@ -1,35 +1,22 @@
 package seedu.nuscents.commands;
 
 import seedu.nuscents.data.TransactionList;
+import seedu.nuscents.data.transaction.Transaction;
 import seedu.nuscents.data.exception.NuscentsException;
-import seedu.nuscents.data.transaction.Allowance;
-import seedu.nuscents.data.transaction.Expense;
 import seedu.nuscents.ui.Ui;
 
 public class EditCommand extends Command {
-    int index;
-    private Expense expense;
-    private Allowance allowance;
-    
-    public EditCommand(Expense expense, int index) {
-        this.expense = expense;
+    private int index;
+    private Transaction transaction;
+
+    public EditCommand(Transaction transaction, int index) {
+        this.transaction = transaction;
         this.index = index;
     }
-    public EditCommand(Allowance allowance, int index) {
-        this.allowance = allowance;
-        this.index = index;
-    }
-    
+
     @Override
     public void execute(TransactionList transactionList) throws NuscentsException {
-        if (expense == null && allowance == null) {
-            throw new NuscentsException("Please enter either an expense or allowance to edit");
-        } else if (expense != null) {
-            transactionList.editTransation(this.index,this.expense);
-        } else{
-            transactionList.editTransation(this.index,this.allowance);
-        }
+        transactionList.editTransaction(this.index, this.transaction);
         Ui.showTransactionList(transactionList);
-
     }
 }
